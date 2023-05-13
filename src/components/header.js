adbab0033645e25a05336a73d2793679ce9337e7
Obuliraj <obuliraj@aaludra.com>
2023-05-13 17:06:02 +0530

    Nav bar changes added

--- src/components/header.js
+++ src/components/header.js
@@ -1,51 +1,102 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import headerlogo from "../assets/header-logo.png";
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import headerlogo from '../assets/header-logo.png'; 
+
 export default function Header() {
-  return <React.Fragment>
-        <Navbar bg="light" expand="lg">
-      <Container className="m-a-0 p-a-0">
-        <Navbar.Brand href="#home"><img src={headerlogo} className="header-logo"  alt="header-logo"/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${""}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${""}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${""}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Activities</Nav.Link>
-                  <Nav.Link href="#action2">Programs & Initiatives</Nav.Link>
-                  <Nav.Link href="#action3">Resources</Nav.Link>
-                  <NavDropdown
-                    title="Our district"
-                    id={`offcanvasNavbarDropdown-expand-${""}`}
-                  >
-                    <NavDropdown.Item href="#action3">Our district</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
-    <div className="">
-    </div>
-  </React.Fragment>;
+  const dropdownMenuItems = [
+    {
+      title: "Our mentor",
+      path: "/Ourmentor",
+    },
+    {
+      title: "Governors team",
+      path: "/Governorsteam",
+    },
+    {
+      title: "Cabinet officials",
+      path: "/Cabinetofficials",
+    },
+    {
+      title: "Governor calendar",
+      path: "/Governorcalendar",
+    },
+    {
+      title: "Intl. President team",
+      path: "/IntlPresident team",
+    },
+    {
+      title: "About us",
+      path: "Aboutus",
+    },
+    {
+      title: "Contact us",
+      path: "Contactus",
+    },
+    {
+      title: "Find a club",
+      path: "Findaclub",
+    },
+    {
+      title: "Become a member",
+      path: "Becomeamember",
+    },
+  ];
+  return (
+    <React.Fragment>
+      <Navbar bg="light" expand="lg">
+        <Container className="m-a-0 p-a-0">
+          <Navbar.Brand href="/">
+            <img src={headerlogo} className="header-logo" alt="header-logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${""}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${""}`}
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${""}`}>
+                <img
+                  src={headerlogo}
+                  className="header-logo"
+                  alt="header-logo"
+                />
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <div className="divider-line mobile-divider"></div>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Activities</Nav.Link>
+                <Nav.Link href="#action2">Programs & Initiatives</Nav.Link>
+                <Nav.Link href="#action3">Resources</Nav.Link>
+                <NavDropdown
+                  title="Our district"
+                  id={`offcanvasNavbarDropdown-expand-${""}`}
+                >
+                  {dropdownMenuItems.map((ele, index) => {
+                    return (
+                      <NavDropdown.Item href={ele.path} key={index}>
+                        {ele.title}
+                      </NavDropdown.Item>
+                    );
+                  })}
+                </NavDropdown>
+                <Nav.Link className="web-menu">Login</Nav.Link>
+                <Nav.Link className="web-menu">Donate</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+            <Nav.Link className="mobile-menu mobile-login-btn">Login</Nav.Link>
+          <Nav.Link className="mobile-menu mobile-donate-btn">Donate</Nav.Link>
+          </Navbar.Offcanvas>
+         
+        </Container>
+      </Navbar>
+      <div className="divider-line"></div>
+    </React.Fragment>
+  );
 }
