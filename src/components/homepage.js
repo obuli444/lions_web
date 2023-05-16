fa00834c997d616628b56bb6d1ccffb4b89595f4
Obuliraj <obuliraj@aaludra.com>
2023-05-16 16:09:42 +0530

    Section three added

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import CarousalSection from '../components/carousalsection';
 import Ourmissionsection from '../components/ourmissionsection';
+import HomeOurleader from '../components/ourleaders';
 export default function Homepage() {
   return <React.Fragment>
     <div className="home-page-section">
     <Ourmissionsection/>
+    <HomeOurleader/>
     </div>
     {/* <CarousalSection/> */}
    
--- src/components/ourleaders.js
+++ src/components/ourleaders.js
@@ -1,1 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import sectionthreedivider from './../assets/sectionthreedivider.png';
+import { useFetchCollection } from "./getfirebasedata";
+
+
+export default function HomeOurleader(){
+  const { fbdbdata: ourleaderssection } =
+    useFetchCollection("ourleaderssection");
+  const { fbdbdata: ourmissionsectiontitle } = useFetchCollection(
+    "ourmissionsectiontitle"
+  );
+  console.log("ourleaderssection", ourmissionsectiontitle);
+  console.log("ourleader", ourleaderssection);
+  return (
+    <React.Fragment>
+      <Box sx={{ flexGrow: 1 }} className="p-t-84">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <img
+              src={sectionthreedivider}
+              alt="divider"
+              className="sectiontwodivider"
+            />
+          </Grid>
+        </Grid>
+        <Grid container className="p-t-40 align-center-section" spacing={0}>
+          <Grid item xs={8}>
+            <p className="section-header-title ">
+              {ourmissionsectiontitle !== null
+                ? ourmissionsectiontitle[0].title
+                : "Our Leader and Governors"}
+            </p>
+          </Grid>
+          <Grid item xs={4} className="text-right">
+            <p className="learnmore">
+              <span>Learn more</span>&nbsp;
+              <span className="material-symbols-outlined learnmore-icon verticalalign-bottom">
+                east
+              </span>
+            </p>
+          </Grid>
+        </Grid>
+        {/* {ourleaderssection !== null &&
+    <Grid container spacing={2}  sx={{ flexGrow: 1 }}>
+         {ourleaderssection.map((ele, index) => {
+            return (
+                <Grid xs={1} sm={1} md={3}  key={index}>
+              <img src={ele.cardimage} alt={ele.name}/>
+              </Grid>
+          
+            );
+          })}
+   </Grid>
+         } */}
+      </Box>
+    </React.Fragment>
+  );
+}
