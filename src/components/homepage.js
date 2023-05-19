6a8fa4846203eb04a899f8bb301a527b1e7a1b33
Obuliraj <obuliraj@aaludra.com>
2023-05-19 15:30:30 +0530

    Carousal Section added

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,18 +1,22 @@
 import React from "react";
-// import CarousalSection from '../components/carousalsection';
+import CarousalSection from '../components/carousalsection';
 import Ourmissionsection from '../components/ourmissionsection';
 import HomeOurleader from '../components/ourleaders';
 import HomeProgramssection from '../components/homeprogramsection';
 import Homecomtributors from '../components/communitycontributors';
 export default function Homepage() {
   return <React.Fragment>
+    <div className="routing-content">
+    <CarousalSection/>
     <div className="home-page-section">
     <Ourmissionsection/>
     <HomeOurleader/>
     <HomeProgramssection/>
     <Homecomtributors/>
     </div>
-    {/* <CarousalSection/> */}
+    </div>
+    
+    
    
   </React.Fragment>;
 }
