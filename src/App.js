5284186dbe03dbf382099b5ca7fd3af0585f01de
Obuliraj <obuliraj@aaludra.com>
2023-05-23 18:37:41 +0530

    footer section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Homepage from './components/homepage';
+import Footer from './components/footer';
 import './style.scss';
 import "primereact/resources/themes/lara-light-indigo/theme.css";     
 import "primereact/resources/primereact.min.css";                                       
@@ -10,6 +11,7 @@ function App() {
   return <React.Fragment>
     <Header/>
     <Homepage/>
+    <Footer/>
   </React.Fragment>
 }
 
