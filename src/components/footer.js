ee14c9bdec852aafc00d2eef937e9f4d772e76ad
Obuliraj <obuliraj@aaludra.com>
2023-05-24 13:37:02 +0530

    Footer link section added

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,17 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useFetchCollection } from "./getfirebasedata";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import playstore from '../assets/playstore.png';
+import  appstore from '../assets/appstore.png';
 export default function Footer() {
   const { fbdbdata: footersectionone } =
     useFetchCollection("footeradsectionone");
-    const { fbdbdata: footersectiontwo } =
+  const { fbdbdata: footersectiontwo } =
     useFetchCollection("footeradsectiontwo");
+  const { fbdbdata: footerlogo } = useFetchCollection("footerlogo");
   return (
     <React.Fragment>
       {/* Sectionone ad with donate content */}
@@ -58,15 +63,200 @@ export default function Footer() {
         </Grid>
       </Box>
       <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} className="footer-grid">
           <Grid item xs={12} sm={12} md={12}>
-          {footersectiontwo !== null && (
+            {footersectiontwo !== null && (
               <img
                 src={footersectiontwo[0].adimage}
                 alt={"footerimage"}
                 width={"100%"}
               />
             )}
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ flexGrow: 1 }} className="footer-sectionone footer-sectiontwo">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={2}>
+            {footerlogo !== null && (
+              <img
+                src={footerlogo[0].logoimage}
+                alt={"footerimage"}
+                width={"54px"}
+              />
+            )}
+            <p className="footer-grid-title p-t-24 m-a-0">Download app </p>
+            <Container fluid>
+              <Row>
+                <Col xs={5} md={12} sm={6} className="p-a-0 p-t-16">
+                  <img src={playstore} alt="playstore"></img>
+                </Col>
+                <Col xs={7} md={12} sm={6} className="p-a-0 p-t-16">
+                  <img src={appstore} alt="appstore"></img>
+                </Col>
+              </Row>
+            </Container>
+          </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <p className="footer-grid-title">International News</p>
+            <Container fluid>
+              <Row>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/footer/lions-press-center"
+                      className="footer-grid-links"
+                    >
+                      Press center
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/blog"
+                      className="footer-grid-links"
+                    >
+                      Service stories
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/blog"
+                      className="footer-grid-links"
+                    >
+                      The lions blog
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://lcoi.in/lm/202302/#p=1"
+                      className="footer-grid-links"
+                    >
+                      The lion magazine
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <p className="footer-grid-title">324 - D</p>
+            <Container fluid>
+              <Row>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Contact us
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Donate us
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Activities
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Resources
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <p className="footer-grid-title">International Media</p>
+            <Container fluid>
+              <Row>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/lions-video-center"
+                      className="footer-grid-links"
+                    >
+                      Video center
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/resources-for-members/resource-center/logos-and-emblems"
+                      className="footer-grid-links"
+                    >
+                      Logos
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a
+                      href="https://www.lionsclubs.org/en/resources-for-members/brand-guidelines"
+                      className="footer-grid-links"
+                    >
+                      Brand guidelines
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <p className="footer-grid-title">Supprot</p>
+            <Container fluid>
+              <Row>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Privacy policy
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Terms of use
+                    </a>
+                  </p>
+                </Col>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                    <a href="/" className="footer-grid-links">
+                      Cookie policy
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ flexGrow: 1 }} className="footer-sectionone footer-sectiontwo">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={2}>
+          <Container fluid>
+              <Row>
+                <Col className="p-a-0" xs={12} md={12} sm={12}>
+                  <p>
+                  </p>
+                </Col>
+                </Row>
+                </Container>
           </Grid>
         </Grid>
       </Box>
